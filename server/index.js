01f135de2a334bdd1588490cb543dac4b584418c
SneakyDragon <fabio.f.dasilva@hotmail.com>
2022-07-02 01:38:56 +0100

    clean up code and add comments

--- server/index.js
+++ server/index.js
@@ -35,39 +35,44 @@ function findFlightWithMostStops(flightDataJson) {
     let flightData = flightDataJson.flights.flight;
     // declare empty array that hold the flights with most stops, because there could potentially be a tie
     var flightsWithMostStops = [];
-    // declare empty arrays that hold the flights with most outgoing and incoming stops, useful for comparing later
 
     flightData.forEach(flight => {
         // if segments are less than 1, there's no stops on this flight, skip code
         if (Object.keys(flight).length > 1) {
             
-            // declare income and outgoing stops.
+            // declare income and outgoing stops. could be useful if we need to distinguish between them
             let outgoingStopsOnFlight = [];
             let incomingStopsOnFlight = [];
             // if flightWithMostStops is empty, means none have been assigned yet, so go ahead and assign them
             if (flightsWithMostStops.length < 1) {
                 flightsWithMostStops.push(flight);
             }
-            // assign length of flights with most stops to a variable for readability
+            // assign number of flights with most stops to a variable for readability
             let mostStopsLength = Object.values(flightsWithMostStops[0].segments[0])[0].length;
             // assign flight segments to a variable for readability
             let flightSegments = Object.values(flight.segments[0])[0];
 
             //loop through the segments
             for(let i = 0; i < flightSegments.length; i++ ){
+                
+                // declare the journey as a variable for readability
+                var journey = Object.values(flightSegments[i])[0].journey;
+
                 //if it's an outward trip, add it to the outgoing flight segments of this trip
-                if(Object.values(flightSegments[i])[0].journey == "out"){
+                if(journey == "out"){
                     outgoingStopsOnFlight.push(Object.values(flightSegments[i])[0]);
                     
                 }//otherwise add it to the incoming
-                else if (Object.values(flightSegments[i])[0].journey == "in"){
+                else if (journey == "in"){
                     incomingStopsOnFlight.push(Object.values(flightSegments[i])[0])
                 }
             }
 
+
+            // add the segments together, to reach a total number of segments
             let stopsThisFlight = incomingStopsOnFlight.length + outgoingStopsOnFlight.length;
             
-            //if the stops from this flight is higher than the trip with most stops (regardless of the position in the array)
+            //if the stops from this flight is higher than the one with most stops (regardless of the position in the array)
             if(stopsThisFlight > mostStopsLength){
                 // pop all of the current items in the array
                 while(flightsWithMostStops.length > 0){
@@ -85,7 +90,6 @@ function findFlightWithMostStops(flightDataJson) {
         
         }
     })
-    //console.log(flightsWithMostStops);
     if(flightsWithMostStops.length > 1){
         console.log("There are currently " + flightsWithMostStops.length + " flights with an equal high amount of stops: ");
     }else{
