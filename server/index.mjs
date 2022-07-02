f6a33e2b71ea3a733f58ac86087de99eb37e6eb0
SneakyDragon <fabio.f.dasilva@hotmail.com>
2022-07-02 02:04:01 +0100

    change file extension to .mjs, include launch.json, replace var with let, replaced require with import

--- server/index.mjs
+++ server/index.mjs
@@ -1,25 +1,29 @@
 
-var fs = require('fs');
-const { IncomingMessage } = require('http');
-xml2js = require('xml2js');
-var watch = require('node-watch');
+import fs from 'fs';
+import { IncomingMessage } from 'http';
+import xml2js from 'xml2js';
+import watch from 'node-watch';
 
-var parser = new xml2js.Parser();
 
-var dataJson;
+let parser = new xml2js.Parser();
+/// flight is misspelled as fligh on the file name
+let flightDataJson;
+let dataJson;
+
+
+
 
 //if the data hasn't been read before, load the data.
 if (!dataJson) {
     initializeData();
 }
-/// flight is misspelled as fligh on the file name
 //Watch the flight data file for changes, if it changes, initializeData again
-watch(__dirname + '/data/flighdata_A.xml', { recursive: true }, function (evt, name) {
+watch('./data/flighdata_A.xml', { recursive: true }, function (evt, name) {
     initializeData();
 });
 
 function initializeData() {
-    dataJson = fs.readFile(__dirname + '/data/flighdata_A.xml', function (err, data) {
+    dataJson = fs.readFile('./data/flighdata_A.xml', function (err, data) {
         parser.parseString(data, function (err, result) {
             findFlightWithMostStops(result);
             console.log('Done');
@@ -30,12 +34,12 @@ function findFlightWithMostStops(flightDataJson) {
     // Read the input from result and shorten syntax for readability purposes
     let flightData = flightDataJson.flights.flight;
     // declare empty array that hold the flights with most stops, because there could potentially be a tie
-    var flightsWithMostStops = [];
+    let flightsWithMostStops = [];
 
     flightData.forEach(flight => {
         // if segments are less than 1, there's no stops on this flight, skip code
         if (Object.keys(flight).length > 1) {
-
+            
             // declare income and outgoing stops. could be useful if we need to distinguish between them
             let outgoingStopsOnFlight = [];
             let incomingStopsOnFlight = [];
@@ -49,14 +53,17 @@ function findFlightWithMostStops(flightDataJson) {
             let flightSegments = Object.values(flight.segments[0])[0];
 
             //loop through the segments
-            for (let i = 0; i < flightSegments.length; i++) {
+            for(let i = 0; i < flightSegments.length; i++ ){
+                
                 // declare the segment as a variable for readability
-                var segment = Object.values(flightSegments[i])[0];
+                let segment = Object.values(flightSegments[i])[0];
+
                 //if it's an outward trip, add it to the outgoing flight segments of this trip
-                if (segment.journey == "out") {
+                if(segment.journey == "out"){
                     outgoingStopsOnFlight.push(segment);
+                    
                 }//otherwise add it to the incoming
-                else if (segment.journey == "in") {
+                else if (segment.journey == "in"){
                     incomingStopsOnFlight.push(segment);
                 }
             }
@@ -64,10 +71,11 @@ function findFlightWithMostStops(flightDataJson) {
 
             // add the segments together, to reach a total number of segments
             let stopsThisFlight = incomingStopsOnFlight.length + outgoingStopsOnFlight.length;
+            
             //if the stops from this flight is higher than the one with most stops (regardless of the position in the array)
-            if (stopsThisFlight > mostStopsLength) {
+            if(stopsThisFlight > mostStopsLength){
                 // pop all of the current items in the array
-                while (flightsWithMostStops.length > 0) {
+                while(flightsWithMostStops.length > 0){
                     flightsWithMostStops.pop();
                 }
                 // then push the flight
@@ -75,16 +83,16 @@ function findFlightWithMostStops(flightDataJson) {
             }
 
             //otherwise simply push to the array
-            if (stopsThisFlight == mostStopsLength) {
+            if (stopsThisFlight == mostStopsLength){
                 flightsWithMostStops.push(flight);
             }
 
-
+        
         }
     })
-    if (flightsWithMostStops.length > 1) {
+    if(flightsWithMostStops.length > 1){
         console.log("There are currently " + flightsWithMostStops.length + " flights with an equal high amount of stops: ");
-    } else {
+    }else{
         console.log("The highest amount of stops is: ");
     }
     console.log(Object.values(flightsWithMostStops[0].segments[0])[0].length);
